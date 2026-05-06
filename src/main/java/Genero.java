e6bd592f2c205798de52f2db29d685403bac531b
chamorro <danielchamorropinto1@gmail.com>
2026-05-06 09:51:20 +0200

    Clase genero

---
+++ src/main/java/Genero.java
@@ -0,0 +1,17 @@
+public class Genero {
+    private String id_genero;
+    private String nombre;
+
+    public String getId_genero() {
+        return id_genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Genero(String id_genero, String nombre) {
+        this.id_genero = id_genero;
+        this.nombre = nombre;
+    }
+}
---
+++ src/main/java/Pelicula.java
@@ -0,0 +1,46 @@
+import exception.VideoclubException;
+
+public class Pelicula {
+    private String id_pelicula;
+    private int año;
+    private String nombre;
+    private int precio;
+    private
+
+    public Pelicula(String id_pelicula, int año, String nombre, int precio) throws VideoclubException {
+        this.id_pelicula = id_pelicula;
+        setAño(año);
+        this.nombre = nombre;
+        setPrecio(precio);
+    }
+
+    public String getId_pelicula() {
+        return id_pelicula;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getRecord() {
+        return precio;
+    }
+
+    public void setAño(int año) throws VideoclubException {
+        if (año < 0 || año > 2026){
+            throw new VideoclubException("El año es incorrecto");
+        }
+        this.año = año;
+    }
+
+    public void setPrecio(int precio) throws VideoclubException {
+        if (precio < 0){
+            throw new VideoclubException("El precio no puede ser negativo");
+        }
+        this.precio = precio;
+    }
+}
